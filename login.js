b4e0d4a0bf722c7f6f74b044f139609a4637b5ab
ayrtonbolwal <ayrtonbolwal@gmail.com>
2023-03-11 10:12:30 -0600

    Cambios en login

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
 console.log("Aquí irá el login");
 console.log("Estes es el segundo cambio de login");
 console.log("Este es el tercer cambio en la rama loign");
+console.log("Este es un código que no será guardado porque cambiaré de rama ya que hubo una emergencia en la otra rama.")
+console.log("Sigo trabajando en ello");

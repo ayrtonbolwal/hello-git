8e8bba68a78176cbc21a6c9fa003e6f34029eb19
ayrtonbolwal <ayrtonbolwal@gmail.com>
2023-03-15 11:23:17 -0600

    Prueba usuario nuevo

--- fichero.js
+++ fichero.js
@@ -1,3 +1,5 @@
 console.log("Hello git");
 console.log("Este es el cambio para el segundo commit!");
 console.log("Esta es una modificación, y el commit 3!");
+
+console.log("Este es un cambioque hacemos desde el otro usuario");
